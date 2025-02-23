311400ec26bddf013cd4bd17b6a79a8c036f6ba6
meherab111 <mehrabborno@gmail.com>
2025-02-24 04:22:23 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const NavbarToggle = document.querySelector(".mobile-nav-toggle");
 
 const primaryNavbar = document.querySelector(".primary-navbar");
 
+// responsive navbar functionality
+
 NavbarToggle.addEventListener("click", () => {
   primaryNavbar.hasAttribute("data-visible")
     ? NavbarToggle.setAttribute("aria-expanded", false)
